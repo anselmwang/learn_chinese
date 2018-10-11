f0d0bf41ae73122c1c21fe8bedf6bdf6f2767343
anselmwang <anselmwang@gmail.com>
2018-10-10 22:15:08 -0700

    * add an web app to quickly remember unfamilar words

---
+++ anki_conn.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+def query(action, params, version=6):
+    r = requests.post('http://127.0.0.1:8765', data = json.dumps({
+            "action": action,
+            "version": 6,
+            "params": params
+        }))
+    return r.json()
+
+def get_learn_state_words():
+    result = query("findNotes",{"query": "deck:Chinese is:learn"})
+    notes = result["result"]
+    result = query("notesInfo", { "notes": notes  })
+    return [x['fields']["Front"]["value"] for x in result["result"]]
+
---
+++ flash_learn/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, redirect, url_for
+import anki_conn
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return redirect(url_for("revise", candidate_str="_".join(anki_conn.get_learn_state_words())))
+
+@app.route('/revise/<candidate_str>')
+def revise(candidate_str):
+    return render_template("index.html", candidate_str=candidate_str)
+
+
+app.config["DEBUG"] = True
+if __name__ == "__main__":
+    app.run(port=8700)
